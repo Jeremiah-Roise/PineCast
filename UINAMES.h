f6362720b26c788cccd233401b397627d11c30c2
Jeremiah <jeremiahroise@outlook.com>
2021-08-12 15:21:08 -0600

    moved UINAMES.h to parent directory

---
+++ UINAMES.h
@@ -0,0 +1,13 @@
+#pragma once
+#define mainPageName "page0"
+#define previewPageName "page1"
+#define notebookName "Lib/Search"
+#define mainStackName "PageSelector"
+#define searchListBoxName "SearchListBox"
+#define podcastDetailsPageName "PodcastDetails"
+#define LibraryUiName "Library"
+#define PVImageName "PVimage"
+#define PVTitleName "PVTitle"
+#define PVAuthorName "PVAuthor"
+#define PVEpisodeListName "PVEpisodeList"
+#define addToLibraryButtonName "addToLib"
