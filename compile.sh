5860bdbd89167982fe3bde0bc1b816f7b9ffb2ee
Jeremiah <jeremiahroise@outlook.com>
2021-08-24 12:12:55 -0600

    include dbus-1

--- compile.sh
+++ compile.sh
@@ -1,1 +1,1 @@
-g++ main.cpp -o main.sh -lcurl -Wall -rdynamic `pkg-config gtk+-3.0 curlpp --libs --cflags` -g
+g++ main.cpp -o main.sh -lcurl -Wall -rdynamic `pkg-config gtk+-3.0 curlpp dbus-1 --libs --cflags` -g
