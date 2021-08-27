eb0117682c0be8ad61a1ec3c25bc3813c4baf401
Jeremiah <jeremiahroise@outlook.com>
2021-08-27 12:49:26 -0600

    added a bit of bash script that does an optimized compile when the first arg is 1

--- compile.sh
+++ compile.sh
@@ -1,1 +1,9 @@
-g++ main.cpp -o main.sh -lcurl -Wall -rdynamic `pkg-config gtk+-3.0 curlpp dbus-1 --libs --cflags` -g
+#!/bin/bash
+if [ "$1" = "1" ]
+then
+	echo "optimized compile"
+	g++ main.cpp -o main.sh -lcurl -Wall -rdynamic `pkg-config gtk+-3.0 curlpp dbus-1 --libs --cflags` -Ofast
+else
+	echo "debugging compile"
+	g++ main.cpp -o main.sh -lcurl -Wall -rdynamic `pkg-config gtk+-3.0 curlpp dbus-1 --libs --cflags` -g
+fi
