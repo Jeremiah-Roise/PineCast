8659c539f0cfae29f450189a01aedac7203c8662
Jeremiah <jeremiahroise@outlook.com>
2021-10-06 18:42:41 -0600

    changed the podcastMetaDataList into a 'typedef vector<podcastMetaData> podcastMetaDataList;'.

--- Libs/PodcastMetaDataLists.h
+++ Libs/PodcastMetaDataLists.h
@@ -16,47 +16,4 @@ string RssFeed;
 int index=0;
 };
 
-
-
-
-class PodcastDataList
-{
-public:
-    int createAndAddPodcast(string author,string RssFeed,string title,string thumbnail30,string thumbnail60,string thumbnail100,string thumbnail600){
-        PodcastData tmp;
-        tmp.artist = author;
-        tmp.RssFeed = RssFeed;
-        tmp.image30 = thumbnail30;
-        tmp.image60 = thumbnail60;
-        tmp.image100 = thumbnail100;
-        tmp.image600 = thumbnail600;
-        tmp.title = title;
-        tmp.index = index;
-        podcasts.push_back(tmp);
-        index ++;
-        return index-1;
-    }
-    PodcastData GetPodcastAtIndex(int index){
-        if (podcasts.empty())
-        {
-            cout << "list is empty" << endl;
-        }
-        
-        return podcasts[index];
-    }
-    int GetIndexSize(){return index;}
-    void clear(){
-        podcasts.clear();
-        index = 0;
-    }
-protected:
-    int index = 0;
-private:
-    std::vector<PodcastData> podcasts;
-    PodcastEpisodeList myEpisodes;
-};
-
-
-
-
-
+typedef vector<PodcastData> PodcastDataList;
