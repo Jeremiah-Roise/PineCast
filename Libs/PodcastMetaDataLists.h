9d51f609f3695bf75b54d57668d217507e3b86ec
Jeremiah <jeremiahroise@outlook.com>
2022-02-22 19:38:11 -0700

    added default values

--- Libs/PodcastMetaDataLists.h
+++ Libs/PodcastMetaDataLists.h
@@ -6,14 +6,14 @@ using namespace std;
 //  conforms to apple podcast search api
 struct PodcastData
 {
-string title;
-string artist;
-string image30;
-string image60;
-string image100;
-string image600;
-string RssFeed;
-size_t index=0;
+string title = "";
+string artist = "";
+string image30 = "";
+string image60 = "";
+string image100 = "";
+string image600 = "";
+string RssFeed = "";
+size_t index = 0;
 };
 
 typedef vector<PodcastData> PodcastDataList;
