4c0701ddfcf79dc918e19aad8962f3b451d3f00c
Jeremiah <jeremiahroise@outlook.com>
2022-02-22 19:37:40 -0700

    added default values

--- Libs/podcastEpisodeTypes.h
+++ Libs/podcastEpisodeTypes.h
@@ -6,14 +6,14 @@ using std::string;
 
     struct PodcastEpisode
     {
-        string title;
-        string artist;
-        string mp3Link;
-        string thumbnailLink;
-        string summary;
-        int episode;
-        string duration;
-        string RssFeed;
+        string title = "";
+        string artist = "";
+        string mp3Link = "";
+        string thumbnailLink = "";
+        string summary = "";
+        int episode = 0;
+        string duration = "";
+        string RssFeed = "";
         int index = 0;
         float Download = 0;
     };
