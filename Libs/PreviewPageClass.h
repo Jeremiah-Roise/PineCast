7a677c86efd1a290b17f1531541183fd24c8e0e9
Jeremiah <jeremiahroise@outlook.com>
2022-05-12 13:03:09 -0600

    fixed issue with "add to library" button showing up even on Podcasts accessed from the library page

--- Libs/PreviewPageClass.h
+++ Libs/PreviewPageClass.h
@@ -31,22 +31,27 @@ class PreviewPageClass
         /// when called it uses the global currentPodcast variable to get the Podcast title, image, artist, etc,
         /// and iterates through the episodes argument to create the episodes,
         /// should probably be updated to not use global variables.
-        void setPreviewPage(PodcastEpisodeList episodes,PodcastData Podcast)
+        void setPreviewPage(PodcastEpisodeList episodes,PodcastData Podcast,bool libraryMode)
         {
             currentPodcast = Podcast;
 
             //  hide the add to library button if in the library
-            //if (page == 0)
-                //gtk_widget_hide(UIPVaddToLibraryButton);
+            if (libraryMode == true)
+                gtk_widget_hide(UIPVaddToLibraryButton);
 
-            //if (page == 1)
-                //gtk_widget_show(UIPVaddToLibraryButton);
+            if (libraryMode == false)
+                gtk_widget_show(UIPVaddToLibraryButton);
 
             gtk_label_set_text(GTK_LABEL(UIPVTitle), currentPodcast.title.c_str());
             gtk_label_set_text(GTK_LABEL(UIPVAuthor), currentPodcast.artist.c_str());
             gtk_image_set_from_pixbuf(GTK_IMAGE(UIPVImage), webTools::createImage(currentPodcast.image600, 200, 200));
 
 
+            if (lastViewedPodcast.title == Podcast.title)
+            {
+                return;
+            }
+            
             clearContainer(GTK_CONTAINER(UIPVEpisodeList));
             GtkWidget* singleEntry;
             for (PodcastEpisode& episode:episodes)
@@ -69,6 +74,7 @@ class PreviewPageClass
                   gtk_container_add(GTK_CONTAINER(UIPVEpisodeList), singleEntry);
               }
             }
+            lastViewedPodcast = Podcast;
         }
 };
 
