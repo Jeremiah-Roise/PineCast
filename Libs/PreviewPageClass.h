fadab28ef3f4c43b2d8ed3ba5491f65386f14fe0
Jeremiah <jeremiahroise@outlook.com>
2022-09-22 14:22:07 -0600

    added support for new audio system. Removed old cout statments

--- Libs/PreviewPageClass.h
+++ Libs/PreviewPageClass.h
@@ -21,16 +21,13 @@ class PreviewPageClass
         GtkWidget* UIPVEpisodeList =      GTK_WIDGET(gtk_builder_get_object(builder, PVEpisodeListName));
         GtkWidget* UIPVPodcastDetailsPage = GTK_WIDGET(gtk_builder_get_object(builder, podcastDetailsPageName));
         GtkWidget* UIPVScrollWindow =       GTK_WIDGET(gtk_builder_get_object(builder, "LibraryScrolledWindow"));
+        AudioPlayer* player;
         PodcastData currentPodcast;
         vector<episodeActionsUI*> episodeActions;// to keep track of episodes currently on the page
         PodcastEpisodeList currentEpisodes;
         void addEpisodesToList(unsigned short int numberOfEpisodesToLoad){
-            cout << "add Episodes function called" << endl;
             for (int i = 0;(i < numberOfEpisodesToLoad && numberLoadedEpisodes < currentEpisodes.size()); i++)
             {
-                cout << "this is the callback" << endl;
-                cout << numberLoadedEpisodes << endl;
-                cout << "printing once" << endl;
               PodcastEpisode episode = currentEpisodes.at(numberLoadedEpisodes);
               PodcastDataBundle dataBundle;
               dataBundle.Episode = episode;
@@ -38,13 +35,13 @@ class PreviewPageClass
 
               if (Downloads::isEpisodeDownloaded(episode))
               {
-                  episodeActionsUI* tmp = new episodeActionsUI(true,dataBundle);
+                  episodeActionsUI* tmp = new episodeActionsUI(true,dataBundle,player);
                   gtk_container_add(GTK_CONTAINER(UIPVEpisodeList), tmp->topBox);
               }
 
               else
               {
-                  episodeActionsUI* tmp = new episodeActionsUI(false,dataBundle);
+                  episodeActionsUI* tmp = new episodeActionsUI(false,dataBundle,player);
                   gtk_container_add(GTK_CONTAINER(UIPVEpisodeList), tmp->topBox);
               }
                 numberLoadedEpisodes++;
@@ -59,7 +56,8 @@ class PreviewPageClass
         }
         unsigned short int numberLoadedEpisodes = 0;
         PodcastData lastViewedPodcast;//    to prevent reloading the current page.
-        PreviewPageClass(GtkBuilder* builder) : builder(builder){
+        PreviewPageClass(GtkBuilder* builder,AudioPlayer* playerToUse) : builder(builder), player(playerToUse)
+        {
           g_signal_connect(UIPVScrollWindow, "edge-reached", (GCallback)callbackLoadMoreEpisodes, (gpointer)this);
           g_signal_connect(UIPVScrollWindow, "edge-overshot",(GCallback)callbackLoadMoreEpisodes, (gpointer)this);
         }
