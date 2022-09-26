550c24969b211f66a103b9e8b12cad499ae31ce1
Jeremiah <jeremiahroise@outlook.com>
2022-09-26 15:05:21 -0600

    removed thumbnail link from episode data type. prettified code.

--- Libs/podcastEpisodeTypes.h
+++ Libs/podcastEpisodeTypes.h
@@ -8,13 +8,12 @@ using std::string;
     {
         string title = "";
         string artist = "";
-        string mp3Link = "";
-        string thumbnailLink = "";
-        string summary = "";
-        int episode = 0;
-        string duration = "";
         string RssFeed = "";
+        string mp3Link = "";
+        string summary = "";
+        string duration = "";
         int index = 0;
+        int episode = 0;
         float Download = 0;
     };
 typedef vector<PodcastEpisode> PodcastEpisodeList;
