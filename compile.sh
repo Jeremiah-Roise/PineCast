2b35fb24611ea0ccc52b96586035edd16c1ed7d8
Jeremiah <jeremiahroise@outlook.com>
2022-10-01 10:20:46 -0600

    use mold linker

--- compile.sh
+++ compile.sh
@@ -2,8 +2,8 @@
 if [ "$1" = "1" ]
 then
 	echo "optimized compile"
-	g++ main.cpp -o main.sh -lcurl -Wall -rdynamic `pkg-config gtk+-3.0 curlpp --libs --cflags` -Ofast
+	g++ main.cpp -o main.sh -lcurl -Wall -rdynamic `pkg-config gtk+-3.0 curlpp --libs --cflags` -fuse-ld=mold -Ofast
 else
 	echo "debugging compile"
-	g++ main.cpp -o main.sh -lcurl -Wall -rdynamic `pkg-config gtk+-3.0 curlpp --libs --cflags` -g
+	g++ main.cpp -o main.sh -lcurl -lpthread -lm -ldl -Wall -rdynamic `pkg-config gtk+-3.0 curlpp --libs --cflags` -fuse-ld=mold -g
 fi
